2294dc54eb2c2b0befafb290749a77754962ebbc
Davis Namsons <davisnamsons@gmail.com>
2019-09-08 01:19:38 +0300

    Add `nilable: true` to root_uri

--- src/lsp/protocol/initialize_params.cr
+++ src/lsp/protocol/initialize_params.cr
@@ -2,7 +2,7 @@ module LSP::Protocol
   struct InitializeParams
     JSON.mapping({
       process_id:   {type: Int64 | Int32 | Nil, key: "processId", nilable: true},
-      root_uri:     {type: String | Nil, key: "rootUri"},
+      root_uri:     {type: String | Nil, key: "rootUri", nilable: true},
       root_path:    {type: String | Nil, key: "rootPath", nilable: true},
       capabilities: JSON::Any,
       trace:        String?,
